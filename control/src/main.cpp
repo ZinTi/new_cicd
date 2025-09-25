38b4b7677d6959583f8788d4083a18a0726cec92
ZinTi <vip201@126.com>
2025-09-25 09:26:02 +0800

    新增条件编译

--- control/src/main.cpp
+++ control/src/main.cpp
@@ -2,5 +2,12 @@
 
 int main() {
     std::cout << "Hello, CLI tools!" << std::endl;
+#ifdef _WIN32
+    std::cout << "Windows OS" << std::endl;
+#elif __linux__
+    std::cout << "Linux OS" << std::endl;
+#else
+    std::cout << "Other OS" << std::endl;
+#endif
     return 0;
 }
--- service/src/hellod.cpp
+++ service/src/hellod.cpp
@@ -5,5 +5,12 @@ int main(int argc, char *argv[]){
     HelloD daemon;
     return daemon.run(argc, argv);*/
     std::cout << "Hello Daemon!" << std::endl;
+#ifdef _WIN32
+    std::cout << "Windows OS" << std::endl;
+#elif __linux__
+    std::cout << "Linux OS" << std::endl;
+#else
+    std::cout << "Other OS" << std::endl;
+#endif
     return 0;
 }
